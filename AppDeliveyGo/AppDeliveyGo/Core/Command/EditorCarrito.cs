f9e0e1d3ac9a6b6e683c4409325d25a8902c9aff
marcosz-96 <marcosz-96@users.noreply.github.com>
2025-09-29 21:59:18 -0300

    Etapa 1 (command)

---
+++ AppDeliveyGo/AppDeliveyGo/Core/Command/EditorCarrito.cs
@@ -0,0 +1,43 @@
+﻿namespace AppDeliveyGo
+{
+    public class EditorCarrito 
+    {
+        private Stack<ICommand> _undoStack = new Stack<ICommand>();
+        private Stack<ICommand> _redoStack = new Stack<ICommand>();
+
+        public void Run(ICommand cmd)
+        {
+            cmd.Execute();
+            _undoStack.Push(cmd);
+            _redoStack.Clear();
+        }
+
+        public void Undo()
+        {
+            if (_undoStack.Count > 0)
+            {
+                ICommand cmd = _undoStack.Pop();
+                cmd.Undo();
+                _redoStack.Push(cmd);
+            }
+            else
+            {
+                Console.WriteLine("[ATENCION] No hay acciones para deshacer");
+            }
+        }
+
+        public void Redo()
+        {
+            if (_redoStack.Count > 0)
+            {
+                ICommand cmd = _redoStack.Pop();
+                cmd.Execute();
+                _undoStack.Push(cmd);
+            }
+            else
+            {
+                Console.WriteLine("[ATENCION] No hay acciones para rehacer");
+            }
+        }
+    }
+}
